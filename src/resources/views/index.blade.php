bd44d7f77309a7c684ab530412066daefeda4b28
LesserPand8 <ken.8.effort@gmail.com>
2025-08-20 22:06:57 +0900

    一時コミット

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+<div class="attendance__alert">
+    // メッセージ機能
+</div>
+@endsection
