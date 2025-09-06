fb328fdf60e5ca9fde5b75994502b46a4985417c
LesserPand8 <ken.8.effort@gmail.com>
2025-09-06 22:08:36 +0900

    commit

--- src/resources/views/sale.blade.php
+++ src/resources/views/sale.blade.php
@@ -4,68 +4,6 @@
 <link rel="stylesheet" href="{{ asset('css/sale.css') }}">
 @endsection
 
-<style>
-    .sale-content {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin-top: 50px;
-    }
-
-    .sale__heading {
-        font-family: Inter;
-        font-weight: 700;
-        font-size: 30px;
-        line-height: 100%;
-        text-align: center;
-        margin-bottom: 30px;
-    }
-
-    .form-group__image {
-        margin: 20px 0;
-    }
-
-    .form-group__label {
-        font-family: Inter;
-        font-weight: 700;
-        font-size: 20px;
-        margin-bottom: 10px;
-        color: #000000;
-    }
-
-    .label {
-        border-bottom: 1.5px solid #5F5F5F;
-        color: #5F5F5F;
-        font-family: Inter;
-        font-weight: 700;
-        font-size: 25px;
-        margin: 20px 0;
-    }
-
-    .form-group__input-image {
-        border: 1px solid #5F5F5F;
-        border-width: 1px;
-        border-radius: 4px;
-        border-style: dashed;
-        width: 100%;
-    }
-
-    .form-group__input-condition {
-        width: 100%;
-        height: 30px;
-        border: 1px solid #5F5F5F;
-        color: #5F5F5F;
-    }
-
-    .condition-label {
-        width: 100%;
-        height: 30px;
-        border: 1px solid #5F5F5F;
-        background: #636769;
-        color: #EAEAEA;
-    }
-</style>
-
 @section('content')
 <div class="sale-content">
     <h2 class="sale__heading">
@@ -135,7 +73,7 @@
                 @enderror
             </div>
             <div class="form-group__label">販売価格</div>
-            <input class="form-group__input-price" type="text" id="price" name="price" value="">
+            <input class="form-group__input-price" type="text" id="price" name="price" value="" placeholder="  ¥ ">
             <div class="form__error">
                 @error('price')
                 {{ $message }}
